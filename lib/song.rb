7c8f5affb4c17f85170e50ca62e449ad36c0a9c7
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-10-09 19:58:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,4 +22,9 @@ def artist_name=(name)
     artist.add_song(self)
   end
   
-
+def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
